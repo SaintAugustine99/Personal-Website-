bef13afc9c88c54b3e2e98aecf90ea81f5994f7d
Onserio Ogeto <onserioogeto@gmail.com>
2026-01-21 14:17:02 +0300

    Update Home page role description to Research, Philosophy, Art

--- my-new-website/frontend/frontend/src/pages/Home.jsx
+++ my-new-website/frontend/frontend/src/pages/Home.jsx
@@ -86,7 +86,7 @@ const Home = () => {
       </NameTitle>
 
       <RoleList>
-        <span>Design</span>
+        <span>Research</span>
         <span>Philosophy</span>
         <span>Art</span>
       </RoleList>
